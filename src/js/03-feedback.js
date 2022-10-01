6f405d1dd6f8c0f5cc01ce3a5075c555b0f1eac1
Gector4 <gector4@gmail.com>
2022-10-01 14:28:11 +0200

    исправил 3 задание

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,34 +1,49 @@
-import throttle from 'lodash.throttle';
-
+const throttle = require('lodash.throttle');
 const form = document.querySelector('.feedback-form');
-const STORAGE_KEY = 'feedback-form-state';
-const formData = {};
+const emailInput = document.querySelector(`input[name="email"]`);
+const messageInput = document.querySelector(`textarea[name="message"]`);
+const LOCALSTORAGE_KEY = 'feedback-form-state';
 
-form.addEventListener('input', throttle(onFormInput, 500));
-form.addEventListener('submit', onFormSubmit);
+updateInput();
 
-updateForm();
+form.addEventListener('input', throttle(onHandlerInput, 500));
+form.addEventListener('submit', onHandlerSubmit);
 
-function onFormInput(event) {
-    formData[event.target.name] = event.target.value;
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+function onHandlerInput(evt) {
+    evt.preventDefault();
+    const user = {
+        email: form.email.value,
+        message: form.message.value,
+    };
+
+    const userString = JSON.stringify(user);
+    localStorage.setItem(LOCALSTORAGE_KEY, `${userString}`);
 }
 
-function onFormSubmit(event) {
-    event.preventDefault();
-    localStorage.removeItem(STORAGE_KEY);
-    event.currentTarget.reset();
-    console.log(formData);
+function updateInput() {
+    let getItemLocalStorage = localStorage.getItem(LOCALSTORAGE_KEY);
+    if (!getItemLocalStorage) return;
+
+    let parseItemLocalStorage = JSON.parse(getItemLocalStorage);
+
+    emailInput.value = parseItemLocalStorage.email || '';
+    messageInput.value = parseItemLocalStorage.message || '';
 }
 
-function updateForm() {
-    let saveInput = localStorage.getItem(STORAGE_KEY);
+function onHandlerSubmit(evt) {
+    evt.preventDefault();
 
-    if (saveInput) {
-        saveInput = JSON.parse(saveInput);
-    } else {
-        return;
+    if (emailInput.value === '' || messageInput.value === '') {
+        return alert('Все поля должны быть заполнены');
     }
 
-    Object.entries(saveInput).forEach(([name, value]) => (form.elements[name].value = value));
+    const user = {
+        email: evt.currentTarget.email.value,
+        message: evt.currentTarget.message.value,
+    };
+
+    console.log(user);
+
+    localStorage.clear();
+    form.reset();
 }
